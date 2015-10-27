bcaf703c85bcaa4a5fc78abdd8343b9075fc1491
lance9980 <lance9980@gmail.com>
2015-10-27 12:07:46 -0700

    js_event_delegation - Lance Seo

--- js_event_delegation/main.js
+++ js_event_delegation/main.js
@@ -1,21 +1,33 @@
 // Answer the questions here:
 
 // - What do you think is going to happen?
+/* When the last added button is clicked, it may display the "Delegated Button #5 Handler" text in the console */
 
 // - What happened?
+/* Clicking the last added button did not console out any message. It did nothing. */
 
 // - Why?
+/* Because the new button was added to the DOM after the feature 1 (Get Button Text) function was loaded, thus the
+   last added button was never applied with the previous function method */
+/* This console outing issue is resolved by moving "addBtn()" function to above the feature 1 operation */
+
 
 //========== Write your code below ===========//
 $("document").ready(function(){
-    $("#list li:first-child").on("click", function(){
-        console.log("Button 1");
+
+    //feature 2 - calls the function to add another button to the DOM
+    addBtn();
+
+    //feature 1 - outputs button text
+    $("#list button").on("click", function(){
+        var btnTxt = $(this).text();
+        console.log(btnTxt);
     });
-    $("#list li:nth-child(2)").on("click", function(){
-        console.log("Button 2");
-    });
+
 });
 
-    function btnClicked() {
-
-    }
+function addBtn() {
+    var newBtn;
+    newBtn = $("<li><button style='margin: 10px'>Delegated Button #5 Handler</button></li>");
+    $("#list").append(newBtn);
+}
