67caa14e3fec92ff785f71104841fe5331d92458
lance9980 <lance9980@gmail.com>
2015-12-01 19:15:35 -0800

    php-sessions error printing..

--- php-sessions/session_reader.php
+++ php-sessions/session_reader.php
@@ -1,1 +1,29 @@
 <!-- Session Reader -->
+<?php
+session_start();
+$form_data = $_POST;
+$_SESSION['f_data'] = $form_data;
+//print_r($_SESSION['f_data']);
+$errArray = array();
+
+if (!preg_match("/^[a-zA-Z ]*$/",$_SESSION['f_data']['name'])) {
+    $nameErr = 'Only letters and white space allowed for NAME';
+    $errArray = ['name'=>$nameErr];
+}
+if (!is_int($_SESSION['f_data']['age'])) {
+    $ageErr = 'Only integers are allowed for AGE';
+    array_push($errArray, ['age'=>$ageErr]);
+}
+if (!preg_match("/^[a-zA-Z ]*$/",$_SESSION['f_data']['occupation'])) {
+    $occuErr = 'Only letters and white space allowed for OCCUPATION';
+    array_push($errArray, ['occu'=>$occuErr]);
+}
+$_SESSION['error'] = $errArray;
+
+//var_dump($_SESSION);
+print_r($_SESSION['error']);
+print_r($_SESSION['error']['name']);
+var_dump(isset($_SESSION['error']['name']));
+?>
+<br><br>
+<a href="session_setter.php">Go to Setter</a><br>
--- php-sessions/session_setter.php
+++ php-sessions/session_setter.php
@@ -1,1 +1,23 @@
 <!-- Session Setter -->
+<?php
+session_start();
+?>
+<br><br>
+<form action="session_reader.php" method="post">
+    <input type="text" name="name" placeholder="name" value="<?=$_SESSION['f_data']['name'];?>"><br>
+    <? if (isset($_SESSION['error']['name'])) {
+        print_r($_SESSION['error']['name']);
+    }
+    ?><br>
+    <input type="text" name="age" placeholder="age" value="<?=$_SESSION['f_data']['age'];?>"><br>
+    <? if (isset($_SESSION['error']['age'])) {
+        print_r($_SESSION['error']['age']);
+    }
+    ?><br>
+    <input type="text" name="occupation" placeholder="occupation" value="<?=$_SESSION['f_data']['occupation'];?>"><br>
+    <? if (isset($_SESSION['error']['occu'])) {
+        print_r($_SESSION['error']['occu']);
+    }
+    ?><br>
+    <input type="submit">
+</form>
